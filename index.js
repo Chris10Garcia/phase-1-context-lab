65af58f8135f4ed323df37e0857700c2264146c3
Christian Garcia <bones192nd@gmail.com>
2023-02-22 16:36:01 -0500

    stuck on wagesEarnedOnDate, needed to leave

--- index.js
+++ index.js
@@ -1,5 +1,67 @@
 /* Your Code Here */
 
+function createEmployeeRecord(array){
+    // first name STRING, family name STRING, title STRING, payrate per hour NUMBER
+    const recordObj = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+    return recordObj    
+}
+
+function createEmployeeRecords(nestedArray){
+    const records = nestedArray.map(array => createEmployeeRecord(array))
+    return records
+}
+
+function createTimeInEvent(dateStamp){
+    const timeInObj = {
+        type: "TimeIn",
+        hour: parseInt(dateStamp.slice(-4),10), // need to count backwards to deal with single or double digit hours and explictly convert to Int
+        date: dateStamp.slice(0,10) // (YYYY-MM-DD)
+    }
+
+    this.timeInEvents.push(timeInObj)   //it's really this simple!!! (no pun intended lol)
+    return this                         // also console log sooner when having issues
+}
+
+function createTimeOutEvent(dateStamp){
+    const timeOutObj = {
+        type: "TimeOut",
+        hour: parseInt(dateStamp.slice(-4),10), // need to count backwards to deal with single or double digit hours and explictly convert to Int
+        date: dateStamp.slice(0,10) // (YYYY-MM-DD)
+    }
+
+    this.timeOutEvents.push(timeOutObj)
+    return this
+}
+
+function hoursWorkedOnDate(dateStamp){
+    // using my last lab, replace employObj with "this" and everything should work
+
+    const timeInValue = this.timeInEvents.find(element => element.date === dateStamp).hour // chaining .hour works here because single element returns
+    const timeOutValue = this.timeOutEvents.find(element => element.date === dateStamp).hour
+
+    return (timeOutValue - timeInValue)/100   //hours is in 100 units. 
+
+}
+
+function wagesEarnedOnDate(dateStamp){
+   
+    const answer = hoursWorkedOnDate(dateStamp)
+
+
+
+    console.log(answer)
+    // return answer
+}
+
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
